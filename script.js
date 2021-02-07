b66592dcb0447d05b1ca34d22ea078e35ac070b6
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-07 01:42:58 -0300

    Ajustando script

---
+++ script.js
@@ -0,0 +1,43 @@
+const Modal = {
+  open() {
+    document.querySelector('.modal-overlay')
+      .classList.add('active')
+  },
+  close() {
+    document.querySelector('.modal-overlay')
+      .classList.remove('active')
+  }
+}
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000, // == -500,00
+    date: '23/01/2021',
+  },
+  {
+    id: 2,
+    description: 'Website',
+    amount: 500000, 
+    date: '23/01/2021',
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021',
+  }
+]
+
+const Transaction = {
+  incomes() {
+    // somar as entradas
+  },
+  expenses() {
+    // somar as saídas
+  },
+  total() {
+    // entradas - saídas
+  }
+}
