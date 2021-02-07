1dc98ba7936cb84ddb7948dda9c69b90d8e106f6
MarcosVel <marcos.veloso@sempreceub.com>
2021-02-07 02:33:35 -0300

    Ajustando amount

--- script.js
+++ script.js
@@ -27,6 +27,12 @@ const transactions = [
     description: 'Internet',
     amount: -20000,
     date: '23/01/2021',
+  },
+  {
+    id: 4,
+    description: 'App',
+    amount: 200000,
+    date: '23/01/2021',
   }
 ]
 
@@ -41,3 +47,50 @@ const Transaction = {
     // entradas - saídas
   }
 }
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/minus.svg" alt="Remover transação">
+      </td>
+    `
+    return html;
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? '-': ''
+
+    value = String(value).replace(/\D/g, '')
+
+    value = Number(value) / 100
+
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+
+    return signal + value;
+  }
+}
+
+transactions.forEach(function(transaction) {
+  DOM.addTransaction(transaction)
+})
